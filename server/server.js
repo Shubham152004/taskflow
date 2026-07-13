32183a8bd89555e1681768acbcbf0766889c7664
Shubham Sharma <shubhamsharmass031@gmail.com>
2026-07-13 18:29:51 +0530

    Update CORS for production

--- server/server.js
+++ server/server.js
@@ -13,7 +13,13 @@ const app =express();
 const PORT =process.env.PORT || 5000;
 
 //Middleware
-app.use(cors());
+app.use(cors(cors({
+    origin: [
+      "http://localhost:5173",
+      "https://taskflow-ebon-alpha.vercel.app",
+    ],
+    credentials: true,
+  })));
 app.use(express.json());
 
 connectDB();
